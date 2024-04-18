f4471a0dca27394bf73828127b41eccdcb7e6e5e
Dinesh-Ande <dinesh2000ande@gmail.com>
2024-04-18 16:02:20 +0530

    code updated with new changes

---
+++ ems-backend/src/main/java/com/java/ems/Constants/EmployeeUrlConstants.java
@@ -0,0 +1,10 @@
+package com.java.ems.Constants;
+
+public class EmployeeUrlConstants {
+    public static final String APIS_BASE_URL = "/apis/employees";
+    public static final String CREATE = "/addEmployee";
+    public static final String GET = "/getEmployee/{id}";
+    public static final String UPDATE = "/updateEmployee/{id}";
+    public static final String DELETE = "deleteEmployee/{id}";
+    public static final String GET_ALL = "/getAll";
+}
--- ems-backend/src/main/java/com/java/ems/Controller/EmployeeController.java
+++ ems-backend/src/main/java/com/java/ems/Controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.java.ems.Controller;
 
+import com.java.ems.Constants.EmployeeUrlConstants;
 import com.java.ems.RequestAndResponse.EmployeeRequestAndResponse;
 import com.java.ems.Service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,36 +12,36 @@ import java.util.List;
 
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/apis/employees")
+@RequestMapping(EmployeeUrlConstants.APIS_BASE_URL)
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @PostMapping("/addEmployee")
+    @PostMapping(EmployeeUrlConstants.CREATE)
     public ResponseEntity<EmployeeRequestAndResponse> saveEmployee(@RequestBody EmployeeRequestAndResponse employeeRequestAndResponse){
         EmployeeRequestAndResponse savedEmployee = employeeService.saveEmployee(employeeRequestAndResponse);
         return  new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
-    @GetMapping("/getEmployee/{id}")
+    @GetMapping(EmployeeUrlConstants.GET)
     public ResponseEntity<EmployeeRequestAndResponse> getEmployeeById(@PathVariable("id") Long employeeId){
         EmployeeRequestAndResponse employeeRequestAndResponse = employeeService.getEmployeeById(employeeId);
         return ResponseEntity.ok(employeeRequestAndResponse);
     }
 
-    @GetMapping("/getAll")
+    @GetMapping(EmployeeUrlConstants.GET_ALL)
     public ResponseEntity<List<EmployeeRequestAndResponse>> getAllEmployees(){
         List<EmployeeRequestAndResponse> employeeRequestAndResponseList = employeeService.getAllEmployees();
         return ResponseEntity.ok(employeeRequestAndResponseList);
     }
 
-    @PutMapping("/updateEmployee/{id}")
+    @PutMapping(EmployeeUrlConstants.UPDATE)
     public ResponseEntity<EmployeeRequestAndResponse> updateEmployeeById(@PathVariable("id") Long employeeId,@RequestBody EmployeeRequestAndResponse employeeRequestAndResponse){
         EmployeeRequestAndResponse updatedEmployee = employeeService.updateEmployeeById(employeeId,employeeRequestAndResponse);
         return ResponseEntity.ok(updatedEmployee);
     }
 
-    @DeleteMapping("deleteEmployee/{id}")
+    @DeleteMapping(EmployeeUrlConstants.DELETE)
     public ResponseEntity<String> deleteEmployeeById(@PathVariable("id") Long employeeId) {
         employeeService.deleteEmployeeById(employeeId);
         return ResponseEntity.ok("Employee Deleted Successfully");
